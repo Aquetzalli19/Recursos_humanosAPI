d60fa56ee69120f890432ea8d672fef1af968391
Emiliano Obregon <aquetzalliobregon@gmail.com>
2022-12-14 20:45:12 -0600

    add Variables

--- DB/models.sql
+++ DB/models.sql
@@ -23,10 +23,3 @@ CREATE TABLE user(
     PRIMARY Key(id)
     
 );
-
-INSERT INTO `employees` (`id`, `name`, `last_name`, `phone`, `email`, `addres`) VALUES
-(1, 'Brian7u7', 'Nutriayapa', '4132349801', 'bryanbona0@gmail.com2', 'Iztapayork2'),
-(2, 'Bryan2', 'Bonilla', '4423464696', 'bryanbona040@gmail.com', 'Tucorazon2'),
-(3, 'Bryan3', 'Bonilla3', '442349864', 'bryanbona04@gmail.com', 'Tucorazon4'),
-(4, 'Bryan4', 'Bonilla4', '552349864', 'bryanbona04@gmail.com', 'Iztapayork'),
-(5, 'Brian', 'Nandayapa', '4132349864', 'bryanbona0@gmail.com', 'Iztapayork2');
--- config/database.js
+++ config/database.js
@@ -1,13 +1,20 @@
 const mysql = require('mysql');
 const util = require('util');
+import { DB_DATABASE,
+    DB_HOST,
+    DB_PORT,
+    DB_USER,
+    DB_PASSWORD,
+} from "../src/config.js";
+
 
 const pool = mysql.createPool({
     connectionLimit:10,
-    host: '127.0.0.1',
-    port: '3306',
-    user: 'root',
-    password: '',
-    database: 'human_resources'
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
 });
 
 pool.query = util.promisify(pool.query);
